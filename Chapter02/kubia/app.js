55bd06a98c8c2514d4a4a7a8c791c2d3095ba174
Xiao Ming Tang <150148347+141680181-myseneca@users.noreply.github.com>
2024-01-29 01:08:15 -0500

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + " from Xiao Ming Tang" + "\n");
+  response.end("You've hit by xmtang1 on  " + os.hostname() + "  from Xiao Ming Tang" + "\n");
 };
 
 var www = http.createServer(handler);
